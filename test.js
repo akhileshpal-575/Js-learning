b6d62b940d5baf11dcdaf66d35c18522cebfb4ec
Akhilesh Pal <palakhilesh466@gmail.com>
2024-01-30 02:06:55 +0530

    add val,const,let

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-console.log("akhilesh pal")
+const accountId = 13433  //good practice to use it
+let accountEmail = "hitesh@google.com" //good to use 
+var accountPassword = "123435"
+accountCity = "jaipur"
+
+// accountId = 2  //not allowed
+console.log(accountId)
+
+// console.table([accountId,account,accountPassword])
