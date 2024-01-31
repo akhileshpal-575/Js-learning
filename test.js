cf109a171fc1f390b708a67ea282d4f73643fea1
Akhilesh Pal <palakhilesh466@gmail.com>
2024-01-31 09:25:12 +0530

    no need

--- test.js
+++
@@ -1,9 +0,0 @@
-const accountId = 13433  //good practice to use it
-let accountEmail = "hitesh@google.com" //good to use 
-var accountPassword = "123435"  //try to avoid use of var
-accountCity = "jaipur"
-
-// accountId = 2  //not allowed
-console.log(accountId)
-
-console.table([accountId,accountEmail,accountPassword]) //data arrange in a tabular format.
