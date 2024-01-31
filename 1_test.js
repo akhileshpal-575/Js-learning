9adbae440f7dbdea799727dfb98c015eaa791284
Akhilesh Pal <palakhilesh466@gmail.com>
2024-01-31 09:21:38 +0530

    data types in js

---
+++ 1_test.js
@@ -0,0 +1,9 @@
+const accountId = 13433  //good practice to use it
+let accountEmail = "hitesh@google.com" //good to use 
+var accountPassword = "123435"  //try to avoid use of var
+accountCity = "jaipur"
+
+// accountId = 2  //not allowed
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword]) //data arrange in a tabular format.
---
+++ 2_Datatypes.js
@@ -0,0 +1,14 @@
+"use  strict"; //treat all js code as newer version
+
+// Datatypes
+// 1.number
+// 2.string
+// 3.boolean
+// 4.null => standalone value
+// 5.undefined
+// 6.symbol
+
+//object
+
+let a  = 76;
+console.log(typeof a); //define type of the data
