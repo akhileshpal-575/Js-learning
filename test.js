58f15a7e77284b97a4e44364bc727508c0696190
Akhilesh Pal <palakhilesh466@gmail.com>
2024-01-31 07:57:42 +0530

    added comment and few lines of code

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 const accountId = 13433  //good practice to use it
 let accountEmail = "hitesh@google.com" //good to use 
-var accountPassword = "123435"
+var accountPassword = "123435"  //try to avoid use of var
 accountCity = "jaipur"
 
 // accountId = 2  //not allowed
 console.log(accountId)
 
-// console.table([accountId,account,accountPassword])
+console.table([accountId,accountEmail,accountPassword]) //data arrange in a tabular format.
