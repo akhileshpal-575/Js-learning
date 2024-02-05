cebf988a9306caf1743ea62505d2816b0aca04f5
Akhilesh Pal <palakhilesh466@gmail.com>
2024-02-05 20:23:52 +0530

    Number in js

---
+++ 8_num_and_maths.js
@@ -0,0 +1,17 @@
+const score = 23949;
+console.log(score)
+
+const balance = new Number(8494);
+console.log(balance)
+
+console.log(balance.toString) //becomes a string
+console.log(balance.toFixed(2))
+
+const otherNumber = 343.98
+console.log(otherNumber.toPrecision(3))
+
+const hundreds = 100000
+console.log(hundreds.toLocaleString('en-IN'))
+
+// ***************Maths******
+
