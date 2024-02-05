856031874778eff5572640a383e98fd553de94a9
Akhilesh Pal <palakhilesh466@gmail.com>
2024-02-05 17:39:36 +0530

    basic operations done

---
+++ 4_Operations.js
@@ -0,0 +1,16 @@
+// ***********************Operations**************
+
+let value = 3
+let negVal = -value;
+// console.log(negVal)
+
+let str1 = "hello"
+let str2 = "hitesh"
+let str3 = str1 + str2
+
+console.log(str3)
+
+let counter = 100
+console.log(++counter) 
+console.log(counter++) 
+console.log(counter)
