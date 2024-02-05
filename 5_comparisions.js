d6c57449c45a48702e44a420d3a2ec8169b22e7f
Akhilesh Pal <palakhilesh466@gmail.com>
2024-02-05 18:08:23 +0530

    basic comparisions

---
+++ 5_comparisions.js
@@ -0,0 +1,9 @@
+// data type should be same for comparing 
+// console.log(null>0) // return false
+// console.log(null == 0) // return false
+// console.log(null >= 0) //here giving true as output because js convert null into 0
+
+// === strict check 
+console.log("2" === 2)
+
+// clean code have more priority
