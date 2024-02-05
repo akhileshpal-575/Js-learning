4f5ee7b8038df895ca07b921608c2afdc884b85e
Akhilesh Pal <palakhilesh466@gmail.com>
2024-02-05 17:19:13 +0530

    type conversion

---
+++ 3_typeConversion.js
@@ -0,0 +1,23 @@
+// let score = "33"
+
+// console.log(typeof score)
+
+// let valueInNum = Number(score)
+// console.log(typeof valueInNum)
+// console.log(valueInNum)
+
+//"33" easily converted into number (33)
+// "33abc" => Nan (but the type becomes number)
+
+// let isLoggedIn = "hitesh";
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn) 
+// console.log(typeof booleanIsLoggedIn)
+// 1 true, 0 false
+// "" = false
+// "hitesh" = true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
