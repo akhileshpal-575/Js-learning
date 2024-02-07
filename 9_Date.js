68e4026696c52c6f0de9881553e49d230144b9da
Akhilesh Pal <palakhilesh466@gmail.com>
2024-02-07 08:17:20 +0530

    added methods of Date

--- 9_Date.js
+++ 9_Date.js
@@ -6,10 +6,24 @@ console.log(myDate.toString()) // readable
 
 //best practice to use 
 console.log(myDate.toDateString()) // show only Date
+console.log(myDate.toLocaleString)
 console.log(typeof myDate)
 
 // month start with zero in javascript
-let myCreateDate = new Date(2024,1,7)
+let myCreateDate = new Date(2024,1,7) 
 console.log(myCreateDate.toDateString())
 
+let myTimeStamp = Date.now()
+console.log(myTimeStamp )
+console.log(myCreateDate.getTime())
+
+// In  milisecond
+console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth)
+console.log(newDate.getDay)
+console.log(newDate.getFullYear)
+console.log(newDate.getTime)
 
