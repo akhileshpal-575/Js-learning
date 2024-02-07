50b07771f85fa429cc4f29918aa0641692ce3f54
Akhilesh Pal <palakhilesh466@gmail.com>
2024-02-07 10:01:38 +0530

    basics of Objects in js

---
+++ 11_Object.js
@@ -0,0 +1,39 @@
+// const obj = new Object()
+
+const regularUser = {
+    fullname: {
+        userfullname : {
+        firstname : "hitesh",
+        surname : "chaudary"
+    }
+  }
+}
+console.log(regularUser.fullname.userfullname.firstname)
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+
+const obj3 = {obj1,obj2}
+console.log(obj3)
+
+const newobj = Object.assign({},obj1,obj2)
+console.log(newobj)
+
+//Array object
+const user =[
+    { email:"hello1.com",
+      name:"akhilesh"
+    },
+    { email:"hello2.com",
+        name:"akhilesh"
+      },
+      { email:"hello3.com",
+        name:"akhilesh"
+      },
+      { email:"hello4.com",
+        name:"akhilesh"
+      },
+          
+]
+user[1].email
+console.log(Object.keys(obj1))
